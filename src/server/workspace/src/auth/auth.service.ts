1b667445dcd24d44ff896ac76ab1bee98e7b4c9b
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-05-29 14:20:15 +0200

    added: auth.service.ts

---
+++ src/server/workspace/src/auth/auth.service.ts
@@ -0,0 +1,29 @@
+// auth.service.ts
+import * as bcrypt from 'bcrypt';
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(email);
+    if (user && await bcrypt.compare(pass, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user._id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
