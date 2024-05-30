bc37b0fd4efedb6e9de35c98acf247e04e62305a
Lunga-Ntando-Ndlovu <u22525697@tuks.co.za>
2024-05-30 21:37:38 +0200

    initial implementation of sidebar component

--- src/client/src/routes/signup/+page.svelte
+++ src/client/src/routes/signup/+page.svelte
@@ -2,12 +2,14 @@
 	import LeftSideImage from '$lib/components/authentication/+LeftSideImage.svelte';
 	import SignUpForm from '$lib/components/authentication/+SignUpForm.svelte';
 	import AuroraBackground from '$lib/components/Utils/+AuroraBackground.svelte';
+	import SideBar from '$lib/components/sidebar/SideBar.svelte';
 	let name = 'World';
 </script>
 
 <main>
 	<div class="flex">
 		<div class="w-1/2">
+			<SideBar/>
 			<SignUpForm />
 		</div>
 		<div class="w-1/2">
