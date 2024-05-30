c4412be6dff28d25ca5c6f76ccf23f35503015c7
joshuawereley <joshuawereley@icloud.com>
2024-05-30 22:42:57 +0200

    Create And List Newly Created Organization Cypress Testing

--- cypress/e2e/adminCreateOrganisation.ts
+++ cypress/e2e/adminCreateOrganisation.ts
@@ -38,4 +38,21 @@ describe("Administrator Creates Organisation", () => {
     cy.url().should("include", "/create-organization");
     cy.contains("Create A New Organization");
   });
+
+  it("should fill out and submit the create organization form", () => {
+    cy.visit("/create-organization");
+    cy.get('input[name="name"]').type(organizationData.name);
+    cy.get('textarea[name="description"]').type(organizationData.description);
+    cy.get('button[type="submit"]').click();
+
+    cy.contains("Organization created successfully").should("exist");
+    cy.url().should("include", "/organizations");
+  });
+
+  it("should list the newly created organization", () => {
+    cy.visit("/organizations");
+
+    cy.contains(organizationData.name).should("exist");
+    cy.contains(organizationData.description).should("exist");
+  });
 });
