c00ec667b7208c0ce55d8273dc5362a98709447c
joshuawereley <joshuawereley@icloud.com>
2024-05-30 22:10:41 +0200

    Register New User Cypress Test

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -10,4 +10,14 @@ describe("Authentication Test", () => {
     cy.get('input[name="email"]').should("exist");
     cy.get('input[name="password"]').should("exist");
   });
+
+  it("should register a new user", () => {
+    cy.visit("register");
+    cy.get('input[name="email"]').type(user.email);
+    cy.get('input[name="password"]').type(user.password);
+    cy.get('input[name="confirmPassword"]').type(user.password);
+    cy.get('button[type="submit"]').click();
+
+    cy.contains("Registration successful").should("exist");
+  });
 });
