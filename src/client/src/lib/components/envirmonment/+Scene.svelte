32606a4cb848abfcb3bfb2cc0442cae61c3ad698
Lunga-Ntando-Ndlovu <u22525697@tuks.co.za>
2024-06-11 19:28:13 +0200

    initial implementation of sandbox

---
+++ src/client/src/lib/components/envirmonment/+Scene.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { T } from "@threlte/core";
+    import {OrbitControls, Sky, GLTF } from "@threlte/extras";
+	import { AmbientLight, AxesHelper, DirectionalLight, Mesh, MeshStandardMaterial, PerspectiveCamera, SphereGeometry } from "three";
+    import CashRegister from "./models/Cash_Register.svelte"
+
+
+	// import { GridHelper } from "three";
+	// import { MeshStandardMaterial, PerspectiveCamera, SphereGeometry } from "three";
+	// import { metalness, roughness } from "three/examples/jsm/nodes/Nodes.js";
+
+</script>
+
+<T.PerspectiveCamera makeDefault position={[0,7,-45]}>
+    <OrbitControls enableDamping={true}/>
+</T.PerspectiveCamera>
+
+
+<!-- Add ambient light for base illumination -->
+<T.AmbientLight intensity={0.9} />
+
+<!-- Add multiple point lights for even lighting -->
+<T.PointLight position={[0, 10, 10]} intensity={0.5} />
+<T.PointLight position={[0, -10, 10]} intensity={0.5} />
+<T.PointLight position={[10, 10, 0]} intensity={0.5} />
+<T.PointLight position={[-10, -10, 0]} intensity={0.5} />
+<T.PointLight position={[0, -10, -10]} intensity={0.5} />
+<T.PointLight position={[0, 0, -10]} intensity={0.5} />
+
+
+<!-- Add a directional light to simulate sunlight -->
+<T.DirectionalLight position={[5, 10, 7]} intensity={0.8} castShadow />
+
+
+
+
+<GLTF url="/models/cash_register_with_a_counting_machine.glb"
+scale={0.1}/>
+
+
+<Sky/>
+<!-- <CashRegister></CashRegister> -->
+
+
+
---
+++ src/client/src/routes/sandbox/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Canvas } from '@threlte/core';
+	import Scene from '../../lib/components/envirmonment/+Scene.svelte';
+</script>
+
+<main>
+	<section class="canvas-wrapper h-[700px] ring">
+		<Canvas>
+			<Scene />
+		</Canvas>
+	</section>
+</main>
