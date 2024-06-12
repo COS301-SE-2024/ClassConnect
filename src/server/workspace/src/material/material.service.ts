97c44f2ee770715dac6cc72fa7523f847d3b0a93
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-06-12 15:57:49 +0200

    added createMaterial service

--- src/server/workspace/src/material/material.service.ts
+++ src/server/workspace/src/material/material.service.ts
@@ -3,3 +3,20 @@
 import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+
+import { Material } from '../schemas/material.schema';
+@Injectable()
+export class MaterialService {
+  constructor(
+    @InjectModel(Material.name)
+    private materialModel: Model<Material>,
+  ) {}
+
+  async create(createMaterialDto: any): Promise<Material> {
+    const createdMaterial = new this.materialModel(
+      createMaterialDto,
+    );
+
+    return createdMaterial.save();
+  }
+}
