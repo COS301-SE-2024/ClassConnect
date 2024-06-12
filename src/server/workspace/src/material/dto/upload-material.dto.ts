ed7f23efb270e91f7161182c6f4af62716f7fd57
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-06-12 16:10:14 +0200

    modified dto

--- src/server/workspace/src/material/dto/upload-material.dto.ts
+++ src/server/workspace/src/material/dto/upload-material.dto.ts
@@ -1,42 +1,15 @@
-import { IsNotEmpty, IsOptional, IsString, IsUrl, isBoolean } from 'class-validator';
+import { IsOptional, IsString, IsUrl } from 'class-validator';
 
-//this class just encapsulates data and does validation
-export class UploadMaterialDto{
+export class UpdateMaterialDto {
+  @IsString()
+  @IsOptional()
+  file_path?: string;
 
-    //this is the type 
-    //either 3d model orpdf
-    @IsNotEmpty()
-    @isBoolean()
-    type: boolean;
+  @IsUrl()
+  @IsOptional()
+  title?: string;
 
-    //this is the workspace  id
-    @IsNotEmpty()
-    @IsString()
-    workspace_id: string;
-
-    //this is the lecturer id
-    @IsNotEmpty()
-    @IsString()
-    lecturer_id: string;
-
-    //this is the title of the material
-    @IsNotEmpty()
-    @IsString()
-    title: string;
-
-    //this is the description of the material
-    @IsNotEmpty()
-    @IsString()
-    description: string;
-
-    //this is the filepath of the material
-    @IsNotEmpty()
-    @IsString()
-    file_path: string;
-
-
-
-    
-
-
+  @IsUrl()
+  @IsOptional()
+  description?: string;
 }
