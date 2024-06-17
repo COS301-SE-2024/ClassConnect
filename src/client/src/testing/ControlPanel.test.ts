8fe7a825343498ff7655380290b00c67ca2d9890
Ashley Kapaso <ashleykapaso@gmail.com>
2024-06-17 16:27:24 +0200

    complete tests

--- src/client/src/testing/ControlPanel.test.ts
+++ src/client/src/testing/ControlPanel.test.ts
@@ -1,15 +1,7 @@
 // test/CallControls.test.js
-import { goto } from '$app/navigation';
-import { render, fireEvent } from '@testing-library/svelte';
-import { describe, it, vi, beforeEach, expect } from 'vitest';
-import { toBeInTheDocument } from '@testing-library/jest-dom/matchers';
+import { render } from '@testing-library/svelte';
+import { describe, it, beforeEach, expect } from 'vitest';
 import ControlPanel from '$lib/components/lesson/ControlPanel.svelte';
-
-vi.mock('$app/navigation', () => ({
-  goto: vi.fn(),
-}));
-
-expect.extend({ toBeInTheDocument });
 
 describe('CallControls', () => {
   let component : any;
@@ -18,17 +10,23 @@ describe('CallControls', () => {
     component = render(ControlPanel);
   });
 
-  it('renders all buttons', () => {
+  it('renders Mic button', () => {
     const { getByText } = component;
     expect(getByText('Mic')).toBeInTheDocument();
-    expect(getByText('Camera')).toBeInTheDocument();
-    expect(getByText('Share Screen')).toBeInTheDocument();
-    expect(getByText('End Call')).toBeInTheDocument();
   });
 
-  it('ends call and navigates to lessons on click', async () => {
-    const endCallButton = component.getByText('End Call');
-    await fireEvent.click(endCallButton);
-    expect(goto).toHaveBeenCalledWith('/lessons');
+  it('renders Camera button', () => {
+    const { getByText } = component;
+    expect(getByText('Camera')).toBeInTheDocument();
+  });
+
+  it('renders Share Screen button', () => {
+    const { getByText } = component;
+    expect(getByText('Share Screen')).toBeInTheDocument();
+  });
+
+  it('renders End Call button', () => {
+    const { getByText } = component;
+    expect(getByText('End Call')).toBeInTheDocument();
   });
 });
