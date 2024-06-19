19ff395f77fc308f07c1b8ffe679b0463fd52587
Gini24mp <u21573558@tuks.co.za>
2024-06-19 16:47:37 +0200

    addition of lecturer dashboard

--- src/client/src/lib/components/lecturer/+Dashboard.svelte
+++ src/client/src/lib/components/lecturer/+Dashboard.svelte
@@ -1,16 +1,14 @@
 <script>
 	import { Tabs, TabItem } from 'flowbite-svelte';
+	import Overview from '$lib/components/lecturer/+Overview.svelte';
+	
 </script>
 
 <Tabs tabStyle="underline">
 	<TabItem open>
 		<div slot="title" class="flex items-center gap-2">Overview</div>
 		<div>
-			<p class="text-sm text-gray-500 dark:text-gray-400">
-				<b>Overview:</b>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua.
-			</p>
+			<Overview />
 		</div>
 	</TabItem>
 
---
+++ src/client/src/lib/components/utils/lecturer/+FilesCard.svelte
@@ -0,0 +1,34 @@
+<script lang='ts'>
+    import IconFiles from '@tabler/icons-svelte/IconFiles.svelte';
+    import UploadStudy from '$lib/components/lecturer/modals/+UploadStudy.svelte';
+
+    const files = [
+        {
+            name: 'Lecture 1',
+        },
+        {
+            name:'Lecture 2',
+        },
+        {
+            name:'Lecture 3',
+        },
+        {
+            name:'Lecture 4',
+        },
+    ]
+
+</script>
+
+<div class="max-w-md rounded-xl border bg-white p-6 pb-10 text-gray-900">
+  <p class="text-lg font-medium">Study Material Uploads</p>
+  {#each files as file}
+    <div class="mt-4 flex items-center rounded-lg bg-gray-100 py-1 px-2 text-gray-600">
+      <IconFiles />
+      <p class="text-sm p-2">{file.name}</p>
+    </div>
+  {/each}
+  <div class="m-2 flex justify-center items-center">
+    <UploadStudy />
+  </div>
+</div>
+  
