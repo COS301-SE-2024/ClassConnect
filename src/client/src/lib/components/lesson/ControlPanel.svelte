e8799ae5fc834e1d84db29e170d72523ba4d5501
Lunga-Ntando-Ndlovu <u22525697@tuks.co.za>
2024-06-20 11:54:43 +0200

    replaced named buttons with icons

--- src/client/src/lib/components/lesson/ControlPanel.svelte
+++ src/client/src/lib/components/lesson/ControlPanel.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import { type Call } from '@stream-io/video-client';
 	import { goto } from '$app/navigation';
+	import { Button } from 'flowbite-svelte';
+	import { MicrophoneOutline, MicrophoneSlashOutline, VideoCameraOutline, ArrowUpFromBracketOutline, PhoneHangupOutline } from 'flowbite-svelte-icons';
+	import {writable} from 'svelte/store';
 
 	export let call: Call;
 
+	const microphoneEnabled = writable(false);
+
 	function toggleMicrophone() {
 		call.microphone.toggle();
+		microphoneEnabled.update(enabled => !enabled);
 	}
 
 	function toggleCamera() {
@@ -22,32 +28,24 @@
 	}
 </script>
 
-<div>
-	<button
-		on:click={toggleMicrophone}
-		class="rounded bg-blue-500 px-2 py-1 font-bold text-white hover:bg-blue-700"
-	>
-		Mic
-	</button>
+<div class="flex space-x-2">
+	<Button pill={true} on:click={toggleMicrophone} class="bg-blue-500 text-white hover:bg-blue-700">
+		{#if $microphoneEnabled}
+			<MicrophoneOutline class="h-6 w-6" />
+		{:else}
+			<MicrophoneSlashOutline class="h-6 w-6" />
+		{/if}
+	</Button>
 
-	<button
-		on:click={toggleCamera}
-		class="rounded bg-blue-500 px-2 py-1 font-bold text-white hover:bg-blue-700"
-	>
-		Camera
-	</button>
+	<Button pill={true} on:click={toggleCamera} class="bg-blue-500 text-white hover:bg-blue-700">
+		<VideoCameraOutline class="h-6 w-6" />
+	</Button>
 
-	<button
-		on:click={toggleScreenShare}
-		class="rounded bg-blue-500 px-2 py-1 font-bold text-white hover:bg-blue-700"
-	>
-		Share Screen
-	</button>
+	<Button pill={true} on:click={toggleScreenShare} class="bg-blue-500 text-white hover:bg-blue-700">
+		<ArrowUpFromBracketOutline class="h-6 w-6" />
+	</Button>
 
-	<button
-		on:click={endCall}
-		class="rounded bg-red-500 px-2 py-1 font-bold text-white hover:bg-red-700"
-	>
-		End Call
-	</button>
+	<Button pill={true} on:click={endCall} class="bg-red-500 text-white hover:bg-red-700 dark:bg-red-500 dark:text-white dark:hover:bg-red-700">
+		<PhoneHangupOutline class="h-6 w-6" />
+	</Button>
 </div>
