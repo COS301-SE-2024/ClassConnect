8bc8a6a5525ba680f4408315e9541b580fbcb509
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-06-22 21:07:29 +0200

    added provisional integration for schedules

---
+++ src/client/src/lib/services/schedule.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+//create Schedule
+//POST req
+export async function schedules(topic: string, userID: string, workspaceID: string, date: string): Promise<any> {
+	try {
+		const response = await axios.post('http://localhost:3000/schedules', {
+			topic: topic,
+			lecture_id: userID,
+			workspace_id: workspaceID,
+            date: date
+		});
+
+		return response.data;
+	} catch (error) {
+		throw new Error('Schedule lesson failed', error);
+	}
+}
