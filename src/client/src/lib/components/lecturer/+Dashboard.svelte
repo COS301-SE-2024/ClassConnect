1b0954af8e89ac539f4a536e2818c2f61122b43c
Gini24mp <u21573558@tuks.co.za>
2024-06-22 18:45:20 +0200

    addition of materials route for the lecturer

--- src/client/src/lib/components/lecturer/+Dashboard.svelte
+++ src/client/src/lib/components/lecturer/+Dashboard.svelte
@@ -1,4 +1,5 @@
 <script>
+	export let files;
 	import { Tabs, TabItem } from 'flowbite-svelte';
 	import Overview from '$lib/components/lecturer/+Overview.svelte';
 </script>
@@ -7,29 +8,8 @@
 	<TabItem open>
 		<div slot="title" class="flex items-center gap-2">Overview</div>
 		<div>
-			<Overview />
+			<Overview files={files}/>
 		</div>
 	</TabItem>
 
-	<TabItem>
-		<div slot="title" class="flex items-center gap-2">Study Material Upload</div>
-		<div>
-			<p class="text-sm text-gray-500 dark:text-gray-400">
-				<b>Study Material:</b>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua.
-			</p>
-		</div>
-	</TabItem>
-
-	<TabItem>
-		<div slot="title" class="flex items-center gap-2">3D Material Upload</div>
-		<div>
-			<p class="text-sm text-gray-500 dark:text-gray-400">
-				<b>3D Material:</b>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua.
-			</p>
-		</div>
-	</TabItem>
 </Tabs>
--- src/client/src/lib/components/utils/lecturer/+ObjectCard.svelte
+++ src/client/src/lib/components/utils/lecturer/+ObjectCard.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
 	import IconCube from '@tabler/icons-svelte/IconCube.svelte';
 	import UploadStudy from '$lib/components/lecturer/modals/+UploadStudy.svelte';
+	export let files;
 
-	const files = [
-		{
-			name: 'Object 1'
-		},
-		{
-			name: 'Object 2'
-		},
-		{
-			name: 'Object 3'
-		},
-		{
-			name: 'Object 4'
-		}
-	];
 </script>
 
 <div class="max-w-md rounded-xl border bg-white p-6 pb-10 text-gray-900">
@@ -23,7 +10,7 @@
 	{#each files as file}
 		<div class="mt-4 flex items-center rounded-lg bg-gray-100 px-2 py-1 text-gray-600">
 			<IconCube />
-			<p class="p-2 text-sm">{file.name}</p>
+			<p class="p-2 text-sm">{file.title}</p>
 		</div>
 	{/each}
 	<div class="m-2 flex items-center justify-center">
