c7ee60d660a00bb7ba7c7f7522c6eece4777ca0f
Joshua Wereley <joshuawereley@Joshuas-MacBook-Pro.local>
2024-06-22 17:04:26 +0200

    Updated Material Integration
    Changed endpoint and include the update workspace.

--- src/client/src/lib/services/materials.ts
+++ src/client/src/lib/services/materials.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function materials(type: boolean, workspace_id: string, lecturer_id: string, title: string, description: string, file_path: string) {
     try {
-        const response = await axios.post('http://localhost:3000/student/module/study/material', {
+        const response = await axios.post('http://localhost:3000/materials', {
             type: type,
             workspace_id: workspace_id,
             lecturer_id: lecturer_id,
@@ -19,9 +19,29 @@ export async function materials(type: boolean, workspace_id: string, lecturer_id
 
 export async function getMaterial(id: string ): Promise<any> {
     try {
-        const response = await axios.get(`http://localhost:3000/study/material/${id}`);
+        const response = await axios.get(`http://localhost:3000/materials/${id}`);
         return response.data;
     } catch (error) {
         throw new Error('Get Material Failed');
+    }
+}
+
+export async function updateWorkspace(
+    id: string,
+    name: string,
+    organisationID: string,
+    users: string[],
+    image: string
+): Promise<any> {
+    try {
+        const response = await axios.put(`http://localhost:3000/materials/${id}`, {
+            name: name,
+            organisationID: organisationID,
+            users: users,
+            image: image
+        });
+        return response.data;
+    } catch (error) {
+        throw new Error('Update Material Failed');
     }
 }
