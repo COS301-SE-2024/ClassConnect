5969d3ec754f8d54d71c1f82c5cc1f9cfb46aae9
Ashley Kapaso <ashleykapaso@gmail.com>
2024-06-28 10:43:31 +0200

    add email sending functioniality

--- src/client/src/routes/(auth)/signup/+page.server.ts
+++ src/client/src/routes/(auth)/signup/+page.server.ts
@@ -1,9 +1,39 @@
 import User from '$db/schemas/User';
+import nodemailer from 'nodemailer';
 import { hash } from '@node-rs/argon2';
+import type { Actions } from './$types';
 import { fail, redirect } from '@sveltejs/kit';
 import { generateUsername } from '$utils/user';
+import { EMAIL_USER, EMAIL_PASS } from '$env/static/private';
 
-import type { Actions } from './$types';
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: EMAIL_USER,
+		pass: EMAIL_PASS
+	}
+});
+
+async function sendWelcomeEmail(to: string, name: string, username: string) {
+	const mailOptions = {
+		from: EMAIL_USER,
+		to: to,
+		subject: 'Welcome To ClassConnect!',
+		html: `
+		<h1>Welcome, ${name}!</h1>
+		<p>Thank you for joining ClassConnect. We're excited to have you on board!</p>
+		<p>Your generated username is: <strong>${username}</strong></p>
+		<p>You can use this username to log in to your account.</p>
+	  `
+	};
+
+	try {
+		await transporter.sendMail(mailOptions);
+		console.log('Welcome email sent successfully');
+	} catch (error) {
+		console.error('Error sending welcome email:', error);
+	}
+}
 
 export const actions: Actions = {
 	default: async (event) => {
@@ -65,10 +95,15 @@ export const actions: Actions = {
 				username,
 				email,
 				role: 'admin',
+				image: 'http://localhost:5173/images/profile-placeholder.png',
 				password: hashedPassword
 			});
 
 			await newUser.save();
+
+			await sendWelcomeEmail(email, name, username);
+
+			throw redirect(302, '/signup/successful');
 		} catch (e) {
 			console.log('Error', e);
 
@@ -76,7 +111,5 @@ export const actions: Actions = {
 				error: 'An unknown error occurred'
 			};
 		}
-
-		redirect(302, '/signin');
 	}
 };
--- src/client/src/routes/(auth)/signup/SideImage.svelte
+++
@@ -1,5 +0,0 @@
-<main>
-	<div class="m2 flex h-screen items-center justify-center">
-		<img alt="Bookcase" class="h-[97%] w-[97%] rounded-xl object-cover" src="/images/bookcase.jpg" />
-	</div>
-</main>
