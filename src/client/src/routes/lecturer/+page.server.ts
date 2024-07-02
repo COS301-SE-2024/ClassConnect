3fb94c560c5765107f9c4a8d7059a939e47fcba6
Ashley Kapaso <ashleykapaso@gmail.com>
2024-07-02 13:22:00 +0200

    add all routes to root

--- src/client/src/routes/lecturer/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { error, redirect } from '@sveltejs/kit';
-
-export async function load({ locals }) {
-	if (!locals.user) redirect(302, '/signin');
-	if (locals.user.role !== 'lecturer') throw error(401);
-
-	return { user: locals.user };
-}
