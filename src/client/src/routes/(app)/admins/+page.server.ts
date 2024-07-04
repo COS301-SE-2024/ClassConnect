2948e16c4617a862cb5d83edad71ca9eb6bd377c
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-07-04 14:38:37 +0200

    added admins load

--- src/client/src/routes/(app)/admins/+page.server.ts
+++ src/client/src/routes/(app)/admins/+page.server.ts
@@ -1,5 +1,37 @@
-import { error } from '@sveltejs/kit';
+import type { Actions } from './$types';
+import { fail, error, redirect } from '@sveltejs/kit';
+
+import User from '$db/schemas/User';
 
 export async function load({ locals }) {
-	if (locals.user.role !== 'admin') throw error(401);
+	//handle user
+	if (!locals.user) return redirect(302, '/signin');
+	if (locals.user.role !== 'admin') throw error(401, 'Unathorised');
+
+
+	//find all admins in organisation
+	try{
+		const admins= await User.find({
+			role: 'admin',
+			organisation: locals.user.organisation
+		}).select('_id name surname username email image');
+	
+
+		const formatAdmins= admins.map((admin)=>(
+			{
+				id: admin._id.toString(),
+				name: admin.name,
+				surname: admin.surname,
+				username: admin.username,
+				email: admin.email,
+				image: admin.image
+			}));
+		return{
+			admins: formatAdmins
+		};
+	}
+	catch (error){
+		console.error('Failed to load data', error);
+		return fail(500, { error: 'An unexpected error occurred while fetching admins' });
+	}
 }
