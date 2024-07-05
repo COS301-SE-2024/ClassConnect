39ea8577322eb80239820ba04589b186b2d7ffcb
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-07-05 17:21:11 +0200

    added announcement modal

--- src/client/src/lib/components/admin/modals/announcement/Add.svelte
+++ src/client/src/lib/components/admin/modals/announcement/Add.svelte
@@ -2,7 +2,6 @@
 	import { enhance } from '$app/forms';
 	import { Button, Modal, Label, Input } from 'flowbite-svelte';
 
-	export let role: string;
 	export let open: boolean;
 
 	let error: string;
@@ -18,3 +17,40 @@
 		};
 	}
 </script>
+
+<Modal bind:open size="xs" class="w-full">
+	<form method="POST" action="?/add" class="flex flex-col" use:enhance={close}>
+		<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Create Announcement</h3>
+
+		{#if error}
+			<p class="mt-2 text-center text-red-500">{error}</p>
+		{/if}
+
+		<Label for="title" class="mb-2 mt-2 text-left">Title</Label>
+		<Input type="text" id="title" name="title" placeholder="Fire Drill" size="md" required />
+
+		<Label for="description" class="mb-2 mt-2 text-left">Description</Label>
+		<Input type="text" id="description" name="description" placeholder="The fire drill is as follows..." size="md" required />
+
+		<Label for="date" class="mb-2 mt-2 text-left">Date</Label>
+		<Input
+			type="date"
+			id="date"
+			name="date"
+			size="md"
+			required
+			placeholder="05/07/2024"
+		/>
+
+		<Label for="image" class="mb-2 mt-4 text-left">Profile Picture</Label>
+		<Input
+			type="text"
+			id="image"
+			name="image"
+			size="md"
+			placeholder="https://example.com/logo.png"
+		/>
+
+		<Button type="submit" class="mt-4 w-full">Add {role}</Button>
+	</form>
+</Modal>
