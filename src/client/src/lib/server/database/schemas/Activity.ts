925537fd4f4d3034d9944a275436d3d5faf6f940
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-07-14 21:56:15 +0200

    fixed students announcements

---
+++ src/client/src/lib/server/database/schemas/Activity.ts
@@ -0,0 +1,33 @@
+import mongoose from '$lib/server/database/db';
+
+
+const activitySchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		required: true,
+		default: Date.now
+	},
+	owner: {
+		type: mongoose.Types.ObjectId,
+		ref:  'Workspace',
+		required: true
+	},
+	createdBy: {
+		type: mongoose.Types.ObjectId,
+		ref: 'User',
+		required: true
+	}
+});
+
+const Activity =
+	mongoose.models.Activity || mongoose.model('Activity', activitySchema);
+
+export default Activity;
