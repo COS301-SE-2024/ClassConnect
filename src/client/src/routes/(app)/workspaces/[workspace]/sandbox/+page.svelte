9d8275dfbdb9e4443666140e9b8302527a536cd3
Lunga-Ntando-Ndlovu <u22525697@tuks.co.za>
2024-07-15 20:30:33 +0200

    addition of tabs

--- src/client/src/routes/(app)/workspaces/[workspace]/sandbox/+page.svelte
+++ src/client/src/routes/(app)/workspaces/[workspace]/sandbox/+page.svelte
@@ -2,11 +2,11 @@
 	import { Canvas } from '@threlte/core';
 	import Scene from '$lib/components/envirmonment/+Scene.svelte';
 	import ObjectSettings from '$lib/components/envirmonment/models/+ObjectSettings.svelte';
-	import { Button, Modal, Label, Input, Fileupload, Radio ,Range,  Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox } from 'flowbite-svelte';
+
+	import { Button, Modal, Label, Input, Fileupload, TableSearch, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox } from 'flowbite-svelte';
 	import { CirclePlusOutline, CloseCircleOutline, WandMagicSparklesOutline, ArrowUpFromBracketOutline, } from 'flowbite-svelte-icons';
 	import { enhance } from '$app/forms';
 	import {items} from "./items"
-	import type { ActionResult } from '@sveltejs/kit';
 	import { displayedSandboxObjectURL } from '$src/lib/store/objects';
 
 	
@@ -17,75 +17,17 @@
 	let zoomToCursor: boolean = false;
 	let zoomSpeed: number = 1;
 	let enableZoom: boolean = true;
-	let sizeRange = 1;
+	let objectSearchTerm = '';
+
+	$: filteredItems = items.filter((item) => item.title.toLowerCase().indexOf(objectSearchTerm.toLowerCase()) !== -1);
 
 	let experimentModal = false;
-	let uploadModal  = false;
-
-    let formErrors = {
-        title: '',
-        description: '',
-        file: ''
-    };
-    let uploadedFileUrl = '';
-	let isValid = true;
 
 	function handleObjectSelect(items:any) {
-		displayedSandboxObjectURL.set(items.fileURL);
-		experimentModal = false;
-		
-	}
-
-	const ALLOWED_EXTENSIONS = ['.glb','.gltf'];
-
-function validateForm(formData: FormData) {
-
-	const file = formData.get('file') as File;
-
-	if (!file || file.size === 0) {
-		formErrors.file = 'Please select a file to upload';
-		isValid = false;
-	} else {
-		const fileName = file.name.toLowerCase();
-		const fileExtension = '.' + fileName.split('.').pop();
-		if (!ALLOWED_EXTENSIONS.includes(fileExtension)) {
-			formErrors.file = `Invalid file type. Allowed types: ${ALLOWED_EXTENSIONS.join(', ')}`;
-			isValid = false;
-		}
-	}
-
-	return isValid;
+	displayedSandboxObjectURL.set(items.fileURL);
+	experimentModal = false;
+	
 }
-
-	function handleTempUpload(event:any) {
-		const form  = event.target as HTMLFormElement;
-		const formData = new FormData(form);
-		
-
-        if (!validateForm(formData)) {
-            event.preventDefault();
-		}
-	}
-		function handleFormSubmit() {
-    return async ({ result }: { result: ActionResult }) => {
-        if (result.type === 'success') {
-            const data = result.data as { success: boolean; fileUrl?: string; error?: string };
-            if (data.success && data.fileUrl) {
-                uploadedFileUrl = data.fileUrl;
-                console.log('Uploaded file URL:', uploadedFileUrl);
-                experimentModal = false;
-            } else {
-                // Handle error
-                console.error('Upload failed:', data.error);
-            }
-        } else if (result.type === 'failure') {
-            console.error('Form submission failed');
-        } else {
-            console.log('Unexpected result type:', result.type);
-        }
-    };
-}
-
 </script>
 
 <main class="p-4">
@@ -94,27 +36,6 @@ function validateForm(formData: FormData) {
 			<h1 class="text-2xl font-semibold dark:text-gray-200">Sandbox</h1>
 			<div class="flex space-x-2">
 
-				<Button on:click={() => (uploadModal = true)} color='blue' class="flex items-center  space-x-1">
-					<ArrowUpFromBracketOutline class="h-5 w-5" />
-					<span>Upload File</span>
-				</Button>
-				<!-- Modal Menu -->
-				<Modal bind:open={uploadModal} size="xs" autoclose={false} class="w-full">
-					<form class="flex flex-col space-y-6" on:submit={handleTempUpload} use:enhance={handleFormSubmit} method="POST" action="?/uploadMat" enctype="multipart/form-data" >
-						<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Upload Student Material</h3>
-						<Label class="space-y-2" for="title">
-							<span>Title</span>
-							<Input type="text" placeholder="title" size="md" id="title" name="title" />
-						</Label>
-						<Label class="space-y-2" for="description">
-							<span>Description</span>
-							<Input type="text" placeholder="description" size="lg" id="description" name="description" />
-						</Label>
-						<Label class="py-2" for="file">Upload File</Label>
-						<Fileupload id="file" size="lg" name="file" />
-						<Button type="submit" class="w-full1">Upload File</Button>
-					</form>
-				</Modal>
 
 
 
@@ -127,7 +48,7 @@ function validateForm(formData: FormData) {
 
 
 		
-	<Table class="my-2" hoverable={true}>
+	<TableSearch class="my-2"  placeholder='Pick a 3D Object' hoverable={true} bind:inputValue={objectSearchTerm}>
 		<TableHead>
 				<TableHeadCell>ID</TableHeadCell>
 				<TableHeadCell>Object</TableHeadCell>
@@ -136,7 +57,7 @@ function validateForm(formData: FormData) {
 		</TableHead>
 
 		<TableBody tableBodyClass="divide-y">
-			{#each items as item}
+			{#each filteredItems as item}
 			<TableBodyRow on:click={() => handleObjectSelect(item)} class="cursor-pointer">
 
 					<TableBodyCell>{item.id}</TableBodyCell>
@@ -148,7 +69,7 @@ function validateForm(formData: FormData) {
 				</TableBodyRow>
 			{/each}
 		</TableBody>
-	</Table>
+	</TableSearch>
 
 				</Modal>
 
