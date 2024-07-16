447855c5a32a8c8837889c1348e437739fcdddc4
Joshua Wereley <joshuawereley@Joshuas-MacBook-Pro.local>
2024-07-16 19:26:34 +0200

    Added isAddModalOpen functionality for button

--- src/client/src/lib/components/workspaces/Admin.svelte
+++ src/client/src/lib/components/workspaces/Admin.svelte
@@ -35,7 +35,7 @@
   <Card img="https://images.inc.com/uploaded_files/image/1920x1080/getty_517610514_353435.jpg" size="none">
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Boost Productivity With Workspaces Now!</h5>
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Create dynamic workspaces to facilitate collaboration between lecturers and students.</p>
-    <Button>
+    <Button on:click={() => (isAddModalOpen = true)}>
       Create Your First Workspace <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
     </Button>
   </Card>
