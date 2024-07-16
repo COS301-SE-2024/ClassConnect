b014db88bafb8b3fc5d131bf6e070defe1eea985
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-07-16 18:10:37 +0200

    added quiz load

--- src/client/src/routes/(app)/workspaces/[workspace]/quizzes/+page.server.ts
+++ src/client/src/routes/(app)/workspaces/[workspace]/quizzes/+page.server.ts
@@ -1,6 +1,36 @@
-import { error, redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+import { fail, error } from '@sveltejs/kit';
+//import { ObjectId } from 'mongodb';
+import mongoose from 'mongoose';
+//import mongoose from 'mongoose';
+import type { ObjectId } from 'mongoose';
+import type { Quiz } from '$src/types';
+import Quizzes from '$db/schemas/Quiz';
 
-export async function load({ locals }) {
-	if (!locals.user) return redirect(302, '/signin');
-	if (locals.user.role !== 'lecturer') throw error(401, 'Unauthorized');
+function formatQuiz(quiz: any): Quiz {
+	return {
+		title: quiz.title,
+		id: quiz._id.toString(),
+		graded: quiz.graded,
+		date: quiz.date.toISOString(),
+	};
+}
+
+async function getQuizzes(ownerID: string | undefined): Promise<Quiz[]> {
+	const quizzes = await Quizzes.find({ owner: ownerID });
+	return quizzes.map(formatQuiz);
+}
+
+export async function load({ params }) {
+	try {
+		const Quizzes = await getQuizzes(params.workspace);
+		console.log('Workspace: ', params.workspace);
+
+		return {
+			Quizzes
+		};
+	} catch (e) {
+		console.error('Failed to load Quizzes: ', e);
+		throw error(500, 'Error occurred while fetching Quizzes');
+	}
 }
