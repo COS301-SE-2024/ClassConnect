e46776f989c6385813877cf0b6f1b17cd4ec33e8
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-07-19 18:08:48 +0200

    ädded setting script

---
+++ src/client/src/lib/components/quizzes/Settings.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { Button, Modal, Label, Input } from 'flowbite-svelte';
+
+	export let open: boolean;
+
+	let error: string;
+
+	function close() {
+		return async ({ result, update }: any) => {
+			if (result.type === 'success') {
+				await update();
+				open = false;
+			} else {
+				error = result.data?.error;
+			}
+		};
+	}
+</script>
