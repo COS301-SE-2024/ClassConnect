7b11298ba95b870413f4d5bad75b3e55c8df776e
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-07-23 18:52:23 +0200

    recovered question page.server

---
+++ src/client/src/routes/(app)/workspaces/[workspace]/question/+page.server.ts
@@ -0,0 +1,7 @@
+
+import { error, redirect } from '@sveltejs/kit';
+
+export async function load({ locals }) {
+	if (!locals.user) return redirect(302, '/signin');
+	if (locals.user.role !== 'lecturer') throw error(401, 'Unauthorized');
+}
