21a9aae9fa68b90740b845506a19cdcaf8448a74
Ashley Kapaso <ashleykapaso@gmail.com>
2024-07-26 18:38:50 +0200

    add loading bar

--- src/client/src/routes/(app)/workspaces/[workspace]/enviroments/sandbox/+page@.svelte
+++ src/client/src/routes/(app)/workspaces/[workspace]/enviroments/sandbox/+page@.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
 	import { Canvas } from '@threlte/core';
+	import { tweened } from 'svelte/motion';
+	import { fade } from 'svelte/transition';
+	import { useProgress } from '@threlte/extras';
 	import Scene from './Scene.svelte';
+
+	const { progress } = useProgress();
+	const tweenedProgress = tweened($progress, { duration: 800 });
+	$: tweenedProgress.set($progress);
 </script>
 
-<main class="h-screen bg-black webgl">
+{#if $tweenedProgress < 1}
+	<div
+		class="absolute inset-0 flex flex-col items-center justify-center gap-1 bg-white text-black"
+		transition:fade|local={{ duration: 200 }}
+	>
+		<p class="text-sm">Loading</p>
+		<div class="relative h-2.5 w-1/3 border border-black">
+			<div class="absolute h-full bg-green-500" style="width: {$tweenedProgress * 100}%" />
+		</div>
+	</div>
+{/if}
+
+<main class="h-screen bg-black">
 	<Canvas>
 		<Scene />
 	</Canvas>
