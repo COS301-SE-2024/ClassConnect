edde3a11ceb2909ae5376d9d3f6daf1f4ad7be79
Ayanda <ms.ayandajuqu@gmail.com>
2024-07-30 18:35:00 +0200

    added grades schema

---
+++ src/client/src/lib/server/database/schemas/Grades.ts
@@ -0,0 +1,27 @@
+import mongoose from '$lib/server/database/db';
+
+const gradesSchema = new mongoose.Schema({
+	studentID: {
+		type: mongoose.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	quizID:{
+        type: mongoose.Types.ObjectId,
+		ref: 'Quiz',
+		required: true
+    },
+	workspaceID:{
+        type: mongoose.Types.ObjectId,
+		ref: 'Workspace',
+		required: true
+    },
+	mark:{
+        type: Number,
+        required: true
+    },
+});
+
+const Grades = mongoose.models.Grades || mongoose.model('Quiz', gradesSchema);
+
+export default Grades;
