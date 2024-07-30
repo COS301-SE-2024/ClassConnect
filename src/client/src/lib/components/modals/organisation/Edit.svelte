5451dc4d3c415002761f9019c2b55faeb2cf9430
bukhosi-eugene-mpande <u21573558@tuks.co.za>
2024-07-30 12:48:03 +0200

    chore: Add enctype attribute to organisation modals

--- src/client/src/lib/components/modals/organisation/Edit.svelte
+++ src/client/src/lib/components/modals/organisation/Edit.svelte
@@ -20,7 +20,7 @@
 </script>
 
 <Modal bind:open size="xs" class="w-full">
-	<form method="POST" action="?/edit" class="flex flex-col" use:enhance={close}>
+	<form method="POST" action="?/edit" class="flex flex-col" use:enhance={close} enctype="multipart/form-data">
 		<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Edit Organisation</h3>
 
 		{#if error}
