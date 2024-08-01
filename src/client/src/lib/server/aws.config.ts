013b6da13da9c82fda3d85962a7ae930b4cf9592
bukhosi-eugene-mpande <u21573558@tuks.co.za>
2024-08-01 17:25:53 +0200

    formating fix

--- src/client/src/lib/server/aws.config.ts
+++ src/client/src/lib/server/aws.config.ts
@@ -1,10 +1,10 @@
 import AWS from 'aws-sdk';
-import { AWS_REGION,AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } from '$env/static/private';
+import { AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } from '$env/static/private';
 
 AWS.config.update({
-    region: AWS_REGION,
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY
+	region: AWS_REGION,
+	accessKeyId: AWS_ACCESS_KEY_ID,
+	secretAccessKey: AWS_SECRET_ACCESS_KEY
 });
 
 export default AWS;
