5ed621a649c93dfb70b5ad174b362de915283619
bukhosi-eugene-mpande <u21573558@tuks.co.za>
2024-08-03 21:17:37 +0200

    chore: Update npm dependencies and fix import statements

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,19 +1,32 @@
-/// <reference types="vitest" />
-
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
+import image from '@rollup/plugin-image';
+import json from '@rollup/plugin-json';
 
 export default defineConfig({
-	plugins: [sveltekit(), svelteTesting()],
-	test: {
-		environment: 'jsdom',
-		setupFiles: ['./vitest-setup.ts'],
-		coverage: {
-			reporter: ['text', 'json', 'html']
-		}
-	},
-	ssr: {
-		noExternal: ['three']
-	}
+  plugins: [
+    sveltekit(),
+    svelteTesting(),
+    image(),
+    json(),
+  ],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest-setup.ts'],
+    coverage: {
+      reporter: ['text', 'json', 'html']
+    }
+  },
+  ssr: {
+    noExternal: ['three']
+  },
+  build: {
+    rollupOptions: {
+      // Exclude bun.lockb or any other binary files
+      external: ['bun.lockb']
+    }
+  }
 });
+
+
