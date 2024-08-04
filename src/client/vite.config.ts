c288840093ef796bd0b87b6c136b5f266128c0c0
bukhosi-eugene-mpande <u21573558@tuks.co.za>
2024-08-04 10:52:45 +0200

    chore: Update npm dependencies and add rollup-plugin-terser and rollup-plugin-visualizer

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -3,6 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import image from '@rollup/plugin-image';
 import json from '@rollup/plugin-json';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { terser } from 'rollup-plugin-terser';
 
 export default defineConfig({
   plugins: [
@@ -10,6 +12,8 @@ export default defineConfig({
     svelteTesting(),
     image(),
     json(),
+    visualizer({ filename: './stats.html' }), // Analyze bundle size
+    terser() // Minify JavaScript
   ],
   test: {
     environment: 'jsdom',
@@ -22,9 +26,17 @@ export default defineConfig({
     noExternal: ['three']
   },
   build: {
+    sourcemap: false, // Remove source maps
     rollupOptions: {
       // Exclude bun.lockb or any other binary files
-      external: ['bun.lockb']
+      external: ['bun.lockb'],
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
     }
   }
 });
