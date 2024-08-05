f76076a88a82e815d7ee37634565f7c6b45e5115
Ayanda <ms.ayandajuqu@gmail.com>
2024-08-05 19:42:49 +0200

    chnaged the [quiz] preview changes

--- src/client/src/routes/(app)/workspaces/[workspace]/quizzes/[quiz]/+page.svelte
+++ src/client/src/routes/(app)/workspaces/[workspace]/quizzes/[quiz]/+page.svelte
@@ -5,18 +5,22 @@
 	import Submission from '$lib/components/modals/quizzes/Submission.svelte';
 	import { browser } from '$app/environment';
 	import { onMount, onDestroy } from 'svelte';
+	import { page } from '$app/stores';
+
 
 	export let data: any;
 	let elapsed = 0;
 	let isFormOpen = false;
 	let activeTimer = false;
+	let isPreview = false;
 	let submissionResult: any = null;
 	function toggleForm() {
 		isFormOpen = !isFormOpen;
 	}
 
 	$: ({ questions, role, duration } = data);
-	$: activeTimer = role === 'student';
+	$: activeTimer = role === 'student' && !isPreview;
+	$: isPreview = $page.url.searchParams.get('preview') === 'true';
 
 	let last_time: number;
 	let frame: number;
@@ -42,10 +46,12 @@
 	}
 
 	onMount(() => {
+		// isPreview = $page.url.searchParams.get('preview') === 'true';
 		if (browser && activeTimer) {
 			last_time = performance.now();
 			update();
 		}
+
 	});
 
 	onDestroy(() => {
@@ -96,20 +102,21 @@
 </script>
 
 <main class="container mx-auto my-8 px-4">
-	<h1 class="mb-4 text-2xl font-bold">Quiz Questions</h1>
-	{#if role === 'student'}
-		{#if questions.length === 0}
-			<p class="text-gray-700 dark:text-gray-300">No questions available for this quiz.</p>
-		{:else}
-			<p class="text-gray-700 dark:text-gray-300">Elapsed time to complete quiz:</p>
-			<Progressbar
-				progress={100 * (1 - elapsed / duration)}
-				size="h-4"
-				color={elapsed / duration > 0.75 ? 'red' : elapsed / duration > 0.5 ? 'yellow' : 'green'}
-			/>
-
-			<div>{(elapsed / 1000).toFixed(1)}s</div>
-			<div class="space-y-6">
+    <h1 class="mb-4 text-2xl font-bold">Quiz Questions</h1>
+    {#if role === 'student' || (role === 'lecturer' && isPreview)}
+        {#if questions.length === 0}
+            <p class="text-gray-700 dark:text-gray-300">No questions available for this quiz.</p>
+        {:else}
+            {#if role === 'student' && !isPreview}
+                <p class="text-gray-700 dark:text-gray-300">Elapsed time to complete quiz:</p>
+                <Progressbar
+                    progress={100 * (1 - elapsed / duration)}
+                    size="h-4"
+                    color={elapsed / duration > 0.75 ? 'red' : elapsed / duration > 0.5 ? 'yellow' : 'green'}
+                />
+                <div>{(elapsed / 1000).toFixed(1)}s</div>
+            {/if}
+            <div class="space-y-6">
 				{#each questions as question (question.id)}
 					<Card>
 						<h2 class="mb-2 text-xl font-bold">Question {question.questionNumber}</h2>
@@ -129,31 +136,32 @@
 							{/each}
 						</div>
 					</Card>
-				{/each}
-
-				<form
-					method="POST"
-					action="?/submitQuiz"
-					use:enhance={() => {
-						return async ({ result }) => {
-							if (result.type === 'success') {
-								submissionResult = result.data;
-							} else {
-								submissionResult = { success: false, error: 'Failed to submit quiz' };
-							}
-							submissionMessage = submissionResult.success
-								? submissionResult.message || 'Quiz submitted successfully'
-								: submissionResult.error || 'An error occurred';
-							submitModalOpen = true;
-						};
-					}}
-				>
-					<input type="hidden" name="mark" value={totalPoints} />
-					<Button type="submit" on:click={handleQuizSubmission}>Submit Quiz</Button>
-				</form>
-			</div>
+					{/each}
+				</div>
+				{#if role === 'student' && !isPreview}
+					<form
+						method="POST"
+						action="?/submitQuiz"
+						use:enhance={() => {
+							return async ({ result }) => {
+								if (result.type === 'success') {
+									submissionResult = result.data;
+								} else {
+									submissionResult = { success: false, error: 'Failed to submit quiz' };
+								}
+								submissionMessage = submissionResult.success
+									? submissionResult.message || 'Quiz submitted successfully'
+									: submissionResult.error || 'An error occurred';
+								submitModalOpen = true;
+							};
+						}}
+					>
+						<input type="hidden" name="mark" value={totalPoints} />
+						<Button type="submit" on:click={handleQuizSubmission}>Submit Quiz</Button>
+					</form>
+				{/if}
 		{/if}
-	{:else if role === 'lecturer'}
+	{:else if role === 'lecturer' && !isPreview}
 		<Form bind:open={isFormOpen} />
 		{#if !isFormOpen}
 			<Button on:click={toggleForm} class="mt-4">Create New Question</Button>
@@ -162,5 +170,4 @@
 		<p class="text-gray-700 dark:text-gray-300">You do not have permission to view this content.</p>
 	{/if}
 </main>
-
 <Submission bind:open={submitModalOpen} {submissionMessage} {totalPoints} />
