abc968556593f86878579f4756d3eb3bfab49566
Ashley Kapaso <ashleykapaso@gmail.com>
2024-08-11 11:37:05 +0200

    add some component tests

--- src/client/src/lib/components/announcements/Card.svelte
+++ src/client/src/lib/components/announcements/Card.svelte
@@ -5,8 +5,14 @@
 	import RemoveModal from '$lib/components/modals/Delete.svelte';
 	import EditModal from '$lib/components/modals/announcement/Edit.svelte';
 
-	export let role;
-	export let announcement;
+	export let role: string;
+	export let announcement: {
+		id: string;
+		title: string;
+		content: string;
+		date: string;
+		createdBy: string;
+	};
 
 	let { id, title, content, date, createdBy } = announcement;
 
@@ -15,13 +21,20 @@
 	let isRemoveModalOpen = false;
 </script>
 
-<div class="my-4">
+<div class="my-4" data-testid="announcement-card">
 	<div class="container rounded-lg bg-white px-4 py-6 shadow-md dark:bg-gray-900 dark:shadow-lg">
 		<div class="flex items-center justify-between">
 			<div class="flex items-center space-x-4">
-				<h1 class="text-2xl font-bold text-gray-900 dark:text-gray-100">{title}</h1>
+				<h1
+					class="text-2xl font-bold text-gray-900 dark:text-gray-100"
+					data-testid="announcement-title"
+				>
+					{title}
+				</h1>
+
 				<span
 					class="rounded-full bg-green-100 px-3 py-1 text-sm text-green-600 dark:bg-green-800 dark:text-green-400"
+					data-testid="announcement-date"
 				>
 					{DATE.toDateString()}
 				</span>
@@ -32,23 +45,35 @@
 					<DotsVerticalOutline
 						size="xl"
 						id="dot-menu"
+						data-testid="dots-menu"
 						class="cursor-pointer text-gray-600 dark:text-gray-300"
 					/>
 
 					<Dropdown placement="bottom-end" triggeredBy="#dot-menu">
-						<DropdownItem on:click={() => (isEditModalOpen = true)}>Edit</DropdownItem>
+						<DropdownItem on:click={() => (isEditModalOpen = true)} data-testid="edit-option">
+							Edit
+						</DropdownItem>
+
 						<DropdownDivider />
-						<DropdownItem on:click={() => (isRemoveModalOpen = true)}>Delete</DropdownItem>
+
+						<DropdownItem on:click={() => (isRemoveModalOpen = true)} data-testid="delete-option">
+							Delete
+						</DropdownItem>
 					</Dropdown>
 				</div>
 			{/if}
 		</div>
 
 		<div class="mt-4">
-			<p class="mt-2 text-xl text-gray-700 dark:text-gray-200">{content}</p>
+			<p class="mt-2 text-xl text-gray-700 dark:text-gray-200" data-testid="announcement-content">
+				{content}
+			</p>
 		</div>
 
-		<div class="mt-2 flex justify-end text-sm text-gray-500 dark:text-gray-400">
+		<div
+			class="mt-2 flex justify-end text-sm text-gray-500 dark:text-gray-400"
+			data-testid="announcement-author"
+		>
 			By: {createdBy}
 		</div>
 	</div>
--- src/client/src/lib/components/common/Banner.svelte
+++ src/client/src/lib/components/common/Banner.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	import { Banner } from 'flowbite-svelte';
 	import { InfoCircleSolid } from 'flowbite-svelte-icons';
-	export let color: string;
+
 	export let type: string;
+	export let color: string;
 	export let message: string;
 </script>
 
 {#if color === 'green'}
 	<Banner color="green" id="cta-banner" position="absolute" bannerType="cta">
 		<div
-			class="mb-2 flex items-center border-gray-200 dark:border-gray-600 md:mb-0 md:me-4 md:border-e md:pe-4"
+			class="mb-2 flex items-center border-gray-200 md:mb-0 md:me-4 md:border-e md:pe-4 dark:border-gray-600"
 		>
 			<InfoCircleSolid color="green" class="me-2 h-6" />
 			<span class="self-center whitespace-nowrap text-lg font-semibold dark:text-white">{type}</span
@@ -20,7 +21,7 @@
 {:else if color === 'red'}
 	<Banner color="red" id="cta-banner" position="absolute" bannerType="cta">
 		<div
-			class="mb-2 flex items-center border-gray-200 dark:border-gray-600 md:mb-0 md:me-4 md:border-e md:pe-4"
+			class="mb-2 flex items-center border-gray-200 md:mb-0 md:me-4 md:border-e md:pe-4 dark:border-gray-600"
 		>
 			<InfoCircleSolid color="red" class="me-2 h-6" />
 			<span class="self-center whitespace-nowrap text-lg font-semibold dark:text-white">{type}</span
@@ -31,7 +32,7 @@
 {:else if color === 'blue'}
 	<Banner color="blue" id="cta-banner" position="absolute" bannerType="cta">
 		<div
-			class="mb-2 flex items-center border-gray-200 dark:border-gray-600 md:mb-0 md:me-4 md:border-e md:pe-4"
+			class="mb-2 flex items-center border-gray-200 md:mb-0 md:me-4 md:border-e md:pe-4 dark:border-gray-600"
 		>
 			<InfoCircleSolid color="blue" class="me-2 h-6" />
 			<span class="self-center whitespace-nowrap text-lg font-semibold dark:text-white">{type}</span
@@ -42,7 +43,7 @@
 {:else if color === 'yellow'}
 	<Banner color="yellow" id="cta-banner" position="absolute" bannerType="cta">
 		<div
-			class="mb-2 flex items-center border-gray-200 dark:border-gray-600 md:mb-0 md:me-4 md:border-e md:pe-4"
+			class="mb-2 flex items-center border-gray-200 md:mb-0 md:me-4 md:border-e md:pe-4 dark:border-gray-600"
 		>
 			<InfoCircleSolid color="yellow" class="me-2 h-6" />
 			<span class="self-center whitespace-nowrap text-lg font-semibold dark:text-white">{type}</span
