78a907e5a45365aa768b6e3582cd731420c02caa
bukhosi-eugene-mpande <u21573558@tuks.co.za>
2024-08-20 19:17:07 +0200

    update to profile page

--- src/src/routes/(app)/profile/+page.svelte
+++ src/src/routes/(app)/profile/+page.svelte
@@ -3,8 +3,11 @@
 	export let data;
 
 	import Profile from '$lib/components/common/Profile.svelte';
+	import Settings from '$lib/components/common/Settings.svelte';
+	
 </script>
 
 <main>
 	<Profile user={data.user_data} />
+	<Settings user={data.user_data} />
 </main>
