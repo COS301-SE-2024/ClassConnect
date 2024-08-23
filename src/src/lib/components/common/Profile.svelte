dde24e6fe2dd5a41b9f431dea00fa00ae103074e
bukhosi-eugene-mpande <u21573558@tuks.co.za>
2024-08-23 19:07:36 +0200

    adddition of new profile page

--- src/src/lib/components/common/Profile.svelte
+++ src/src/lib/components/common/Profile.svelte
@@ -1,10 +1,57 @@
 <script lang="ts">
-	import { Button } from 'flowbite-svelte';
+	import { Button, Modal, Gallery } from 'flowbite-svelte';
+	import DeleteProfilePic from '$lib/components/modals/settings/DeleteProfilePic.svelte';
+	import UploadPicture from '$lib/components/modals/settings/UploadPicture.svelte';
+	import Settings from '$lib/components/common/ProfileSettings.svelte';
+	import { change } from '$lib/store';
+	import axios from 'axios';
+	import { onMount } from 'svelte';
 	export let user: any;
+
+	let openEditProfile = false;
+	let openDeleteModal = false;
+	let openFileHandlingModal = false;
+	let update = false;
+
+	const apiUrl = '/api/user';
+
+	async function getUserData() {
+		try {
+			axios
+				.get(apiUrl)
+				.then((response) => {
+					console.log('User data:', response.data);
+					user = response.data;
+				})
+				.catch((error) => {
+					if (error.response) {
+						console.error('Error response data:', error.response.data);
+						console.error('Error response status:', error.response.status);
+					} else if (error.request) {
+						console.error('Error request:', error.request);
+					} else {
+						console.error('Error message:', error.message);
+					}
+				});
+		} catch (error) {
+			console.error('There was a problem with the get operation:', error);
+		}
+	}
+
+	onMount(() => {
+		getUserData();
+	});
+
+	$: {
+		change.subscribe(() => {
+			getUserData();
+		});
+	}
+	
 </script>
 
-<div class="ease-soft-in-out xl:ml-68.5 relative h-full bg-gray-50 transition-all duration-200">
-	<section class="h-full w-full overflow-hidden dark:bg-gray-900">
+<div class="ease-soft-in-out xl:ml-68.5 relative h-full transition-all duration-200">
+	<section class="h-full w-full dark:bg-gray-900">
 		<div class="mx-auto h-full w-full">
 			<!-- User Cover IMAGE -->
 			<img
@@ -16,6 +63,7 @@
 			<div class="mx-auto flex w-full justify-center">
 				<img
 					src={user.image}
+					on:click={() => (openEditProfile = true)}
 					alt="User Profile"
 					class="xs:w-[8rem] xs:h-[8rem] xs:bottom-[4.3rem] relative rounded-full object-cover shadow-xl outline outline-2 outline-offset-2 outline-green-500 sm:bottom-[5rem] sm:h-[10rem] sm:w-[10rem] md:bottom-[6rem] md:h-[12rem] md:w-[12rem] lg:bottom-[8rem] lg:h-[16rem] lg:w-[16rem] xl:bottom-[7rem] xl:h-[16rem] xl:w-[16rem]"
 				/>
@@ -33,12 +81,12 @@
 				<div
 					class="overflow-hidden rounded-lg border bg-white shadow dark:border-gray-700 dark:bg-gray-800"
 				>
-					<div class="px-4 py-5 sm:px-6">
+					<div class="px-2 py-5 sm:px-6">
 						<h3 class="text-center text-lg font-medium leading-6 text-gray-900 dark:text-white">
 							Profile information
 						</h3>
 					</div>
-					<div class="border-t border-gray-200 px-4 py-5 dark:border-gray-600 sm:p-0">
+					<div class="border-t border-gray-200 px-4 py-2 dark:border-gray-600 sm:p-0">
 						<dl class="sm:divide-y sm:divide-gray-200 dark:sm:divide-gray-600">
 							<div class="py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 sm:py-5">
 								<dt class="text-sm font-medium text-gray-500 dark:text-white">Full name :</dt>
@@ -58,9 +106,48 @@
 
 				<!-- Update Link -->
 				<div class="flex rounded-sm px-2">
-					<Button href="/settings">Update Details</Button>
+					<Button on:click={() => (update = !update)}>Update Details</Button>
 				</div>
+
 			</div>
 		</div>
 	</section>
 </div>
+
+<!-- Edit Modal -->
+<Modal id="deleteModal" bind:open={openEditProfile} size="md" placement="center">
+	<form
+		class="p-6 text-center"
+	>
+		<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+			Edit Profile Picture
+		</h3>
+		<div class="flex justify-center">
+			<Gallery class="gap-4 py-2">
+				<img class="h-96 w-96 rounded-full" src={user.image} alt='Proffile' />
+			</Gallery>
+		</div>
+		<div class="flex justify-center gap-4">
+    		<Button on:click={() => {
+                openEditProfile = false;
+                openFileHandlingModal = true;
+            }}>Upload</Button>
+    		<Button color='red' on:click={() => {
+                openEditProfile = false;
+                openDeleteModal = true;
+            }}>Delete</Button>
+			<Button color='alternative' on:click={() => (openEditProfile = false)}>Cancel</Button>
+		</div>
+	</form>
+</Modal>
+
+<!-- Delete Modal -->
+<DeleteProfilePic bind:open={openDeleteModal} />
+
+<!-- File Handling Modal -->
+<UploadPicture bind:open={openFileHandlingModal} />
+
+<!-- Update forms -->
+ {#if update}
+	<Settings user={user} />
+ {/if}
--- src/src/routes/(app)/profile/+page.svelte
+++ src/src/routes/(app)/profile/+page.svelte
@@ -3,11 +3,9 @@
 	export let data;
 
 	import Profile from '$lib/components/common/Profile.svelte';
-	import Settings from '$lib/components/common/Settings.svelte';
 	
 </script>
 
 <main>
 	<Profile user={data.user_data} />
-	<Settings user={data.user_data} />
 </main>
