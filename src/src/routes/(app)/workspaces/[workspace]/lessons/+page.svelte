83281215de9ac573714d64e00bdbb1abe83bcd3a
Joshua Wereley <joshuawereley@Joshuas-MacBook-Pro.local>
2024-09-03 12:28:54 +0200

    Updated Styling for Lessons table

--- src/src/routes/(app)/workspaces/[workspace]/lessons/+page.svelte
+++ src/src/routes/(app)/workspaces/[workspace]/lessons/+page.svelte
@@ -1,146 +1,90 @@
-<script>
-  import { onMount } from 'svelte';
-  import { Button, Tabs, TabItem, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, TableSearch, Button, Dropdown, DropdownItem, Checkbox, ButtonGroup } from 'flowbite-svelte';
-  import { Section } from 'flowbite-svelte-blocks';
-  import paginationData from '../utils/advancedTable.json'
-  import { PlusOutline, ChevronDownOutline, FilterSolid, ChevronRightOutline, ChevronLeftOutline } from 'flowbite-svelte-icons';
+<script lang="ts">
+	import { Button, Tabs, TabItem } from 'flowbite-svelte';
 
-  let divClass='bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden';
-  let innerDivClass='flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4';
-  let searchClass='w-full md:w-1/2 relative';
-  let svgDivClass='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none';
-  let classInput="text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2  pl-10";
+	import { formatDate } from '$utils/date';
+	import Upcoming from '$src/lib/components/lessons/Upcoming.svelte';
+	import InSession from '$src/lib/components/lessons/InSession.svelte';
+	import Recordings from '$src/lib/components/lessons/lesson/Recordings.svelte';
+	import ScheduleModal from '$src/lib/components/modals/lessons/Schedule.svelte';
 
-  let searchTerm = '';
-  let currentPosition = 0;
-  const itemsPerPage = 10;
-  const showPage = 5;
-  let totalPages = 0;
-  let pagesToShow = [];
-  let totalItems = paginationData.length;
-  let startPage;
-  let endPage;
+	export let data: any;
 
-  const updateDataAndPagination = () => {
-    const currentPageItems = paginationData.slice(currentPosition, currentPosition + itemsPerPage);
-    renderPagination(currentPageItems.length);
-  }
+	let isScheduleModalOpen = false;
 
-  const loadNextPage = () => {
-    if (currentPosition + itemsPerPage < paginationData.length) {
-      currentPosition += itemsPerPage;
-      updateDataAndPagination();
-    }
-  }
+	let upcomingLessons: any[] = [];
+	let inSessionLessons: any[] = [];
 
-  const loadPreviousPage = () => {
-    if (currentPosition - itemsPerPage >= 0) {
-      currentPosition -= itemsPerPage;
-      updateDataAndPagination();
-    }
-  }
+	$: ({ role, lessons, recordings } = data);
 
-  const renderPagination = (totalItems) => {
-    totalPages = Math.ceil(paginationData.length / itemsPerPage);
-    const currentPage = Math.ceil((currentPosition + 1) / itemsPerPage);
+	$: {
+		const now = new Date();
 
-    startPage = currentPage - Math.floor(showPage / 2);
-    startPage = Math.max(1, startPage);
-    endPage = Math.min(startPage + showPage - 1, totalPages);
+		lessons = lessons.map((lesson: any) => ({
+			...lesson,
+			date: formatDate(new Date(lesson.date)),
+			unformattedDate: new Date(lesson.date)
+		}));
 
-    pagesToShow = Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
-  }
-
-  const goToPage = (pageNumber) => {
-    currentPosition = (pageNumber - 1) * itemsPerPage;
-    updateDataAndPagination();
-  }
-
-  $: startRange = currentPosition + 1;
-  $: endRange = Math.min(currentPosition + itemsPerPage, totalItems);
-
-  onMount(() => {
-    // Call renderPagination when the component initially mounts
-    renderPagination(paginationData.length);
-  });
-
-  $: currentPageItems = paginationData.slice(currentPosition, currentPosition + itemsPerPage);
-  $: filteredItems = paginationData.filter((item) => item.product_name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1);
+		upcomingLessons = lessons.filter((lesson: any) => lesson.unformattedDate > now);
+		inSessionLessons = lessons.filter((lesson: any) => lesson.unformattedDate <= now);
+	}
 </script>
 
-<Section name="advancedTable" classSection='bg-gray-50 dark:bg-gray-900 p-3 sm:p-5'>
-    <TableSearch placeholder="Search" hoverable={true} bind:inputValue={searchTerm} {divClass} {innerDivClass} {searchClass} {classInput} >
+<Tabs>
+	<TabItem title="Lessons" open>
+		<main class="container mx-auto my-2 px-4">
+			<div class="sm:flex sm:items-center sm:justify-between">
+				<div>
+					<div class="flex items-center gap-x-3">
+						<h2 class="text-xl font-bold text-gray-800 dark:text-white">Lessons</h2>
+						<span
+							class="rounded-full bg-green-100 px-3 py-1 text-xs text-green-600 dark:bg-gray-800 dark:text-green-400"
+						>
+							{lessons.length} lessons
+						</span>
+					</div>
+				</div>
+				{#if role === 'lecturer'}
+					<Button size="sm" on:click={() => (isScheduleModalOpen = true)}>Schedule Lesson</Button>
+				{/if}
+			</div>
+			<br />
 
-    <div slot="header" class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-      <Button>
-        <PlusOutline class="h-3.5 w-3.5 mr-2" />Schedule Lesson
-      </Button>
-      <Button color='alternative'>Actions<ChevronDownOutline class="w-3 h-3 ml-2 " /></Button>
-        <Dropdown class="w-44 divide-y divide-gray-100">
-          <DropdownItem>Mass Edit</DropdownItem>
-          <DropdownItem>Delete all</DropdownItem>
-        </Dropdown>
-      <Button color='alternative'>Filter<FilterSolid class="w-3 h-3 ml-2 " /></Button>
-        <Dropdown class="w-48 p-3 space-y-2 text-sm">
-          <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">Choose brand</h6>
-          <li>
-            <Checkbox>Apple (56)</Checkbox>
-          </li>
-          <li>
-            <Checkbox>Microsoft (16)</Checkbox>
-          </li>
-          <li>
-            <Checkbox>Razor (49)</Checkbox>
-          </li>
-          <li>
-            <Checkbox>Nikon (12)</Checkbox>
-          </li>
-          <li>
-            <Checkbox>BenQ (74)</Checkbox>
-          </li>
-        </Dropdown>
-    </div>
-      <TableHead>
-        <TableHeadCell padding="px-4 py-3" scope="col">Topic</TableHeadCell>
-        <TableHeadCell padding="px-4 py-3" scope="col">Date</TableHeadCell>
-        <TableHeadCell padding="px-4 py-3" scope="col">Starting Time</TableHeadCell>
-        <TableHeadCell padding="px-4 py-3" scope="col">Ending Time</TableHeadCell>
-      </TableHead>
-      <TableBody class="divide-y">
-        {#if searchTerm !== ''}
-          {#each filteredItems as item (item.id)}
-            <TableBodyRow>
-              <TableBodyCell tdClass="px-4 py-3">{item.}</TableBodyCell>
-              <TableBodyCell tdClass="px-4 py-3">{item.brand}</TableBodyCell>
-              <TableBodyCell tdClass="px-4 py-3">{item.category}</TableBodyCell>
-              <TableBodyCell tdClass="px-4 py-3">{item.price}</TableBodyCell>
-            </TableBodyRow>
-          {/each}
-        {:else}
-          {#each currentPageItems as item (item.id)}
-            <TableBodyRow>
-              <TableBodyCell tdClass="px-4 py-3">{item.product_name}</TableBodyCell>
-              <TableBodyCell tdClass="px-4 py-3">{item.brand}</TableBodyCell>
-              <TableBodyCell tdClass="px-4 py-3">{item.category}</TableBodyCell>
-              <TableBodyCell tdClass="px-4 py-3">{item.price}</TableBodyCell>
-            </TableBodyRow>
-          {/each}
-        {/if}
-      </TableBody>
-      <div slot="footer" class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4" aria-label="Table navigation">
-      <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
-        Showing
-        <span class="font-semibold text-gray-900 dark:text-white">{startRange}-{endRange}</span>
-        of
-        <span class="font-semibold text-gray-900 dark:text-white">{totalItems}</span>
-      </span>
-        <ButtonGroup>
-          <Button on:click={loadPreviousPage} disabled={currentPosition === 0}><ChevronLeftOutline size='xs' class='m-1.5'/></Button>
-          {#each pagesToShow as pageNumber}
-            <Button on:click={() => goToPage(pageNumber)}>{pageNumber}</Button>
-          {/each}
-          <Button on:click={loadNextPage} disabled={ totalPages === endPage }><ChevronRightOutline size='xs' class='m-1.5'/></Button>
-        </ButtonGroup>
-      </div>
-    </TableSearch>
-</Section>
+			{#if lessons.length === 0}
+				<p class="text-l text-gray-800 dark:text-white">
+					There are no lessons scheduled at the moment.
+				</p>
+			{:else}
+				<InSession lessons={inSessionLessons} {role} />
+				<Upcoming lessons={upcomingLessons} {role} />
+			{/if}
+		</main>
+	</TabItem>
+	<TabItem title="Recordings">
+		<main class="container mx-auto my-2 px-4">
+			<div class="sm:flex sm:items-center sm:justify-between">
+				<div>
+					<div class="flex items-center gap-x-3">
+						<h2 class="text-xl font-bold text-gray-800 dark:text-white">Recordings</h2>
+						<span
+							class="rounded-full bg-green-100 px-3 py-1 text-xs text-green-600 dark:bg-gray-800 dark:text-green-400"
+						>
+							{recordings.length} recordings
+						</span>
+					</div>
+				</div>
+			</div>
+			<br />
+
+			{#if recordings.length === 0}
+				<p class="text-l text-gray-800 dark:text-white">
+					There are no recordings of the lessons at the moment.
+				</p>
+			{:else}
+				<Recordings {role} lessons={recordings} />
+			{/if}
+		</main>
+	</TabItem>
+</Tabs>
+
+<ScheduleModal bind:open={isScheduleModalOpen} />
