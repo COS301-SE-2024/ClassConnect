d32efd8db4406ceb078e75ed34d71d1f9eede0c3
bukhosi-eugene-mpande <u21573558@tuks.co.za>
2024-09-27 17:42:51 +0200

    formating fix

--- src/src/lib/components/workspaces/workspace/Card.svelte
+++ src/src/lib/components/workspaces/workspace/Card.svelte
@@ -25,7 +25,7 @@
 	<div class="p-4">
 		<h5 class="mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
 		<p class="mb-2 line-clamp-2 text-sm text-gray-700 dark:text-gray-400">{description}</p>
-		<Button size="sm" on:click={openWorkspace} >
+		<Button size="sm" on:click={openWorkspace}>
 			Open <ArrowRightOutline class="ms-2 h-4 w-4" />
 		</Button>
 	</div>
